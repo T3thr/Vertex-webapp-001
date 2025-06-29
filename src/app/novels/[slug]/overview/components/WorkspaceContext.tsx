657a2cce1e06daae6276db5c89da3168b906c20c
T3thr <t.theerapat33@gmail.com>
2025-06-30 01:11:22 +0700

    solve lodash dependency error

--- src/app/novels/[slug]/overview/components/WorkspaceContext.tsx
+++ src/app/novels/[slug]/overview/components/WorkspaceContext.tsx
@@ -13,7 +13,14 @@ import {
   MarkerType,
   XYPosition
 } from 'reactflow';
-import { debounce } from 'lodash';
+// Custom debounce function to avoid lodash dependency
+function debounce<T extends (...args: any[]) => any>(func: T, wait: number): T {
+  let timeout: NodeJS.Timeout;
+  return ((...args: any[]) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  }) as T;
+}
 
 // Types
 import { NovelData, EpisodeData, StoryMapData } from '../page';
